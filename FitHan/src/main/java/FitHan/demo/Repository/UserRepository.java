f654af8b5daf8e88d233e2c3e64438b928c4e8c3
Gwonyi Moon <102517739+M00NPANG@users.noreply.github.com>
2024-12-01 05:54:52 +0900

    User Repository - sava, 전화번호로 찾기 기능

---
+++ FitHan/src/main/java/FitHan/demo/Repository/UserRepository.java
@@ -0,0 +1,17 @@
+package FitHan.demo.Repository;
+
+import FitHan.demo.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User save(User user);
+
+    User findByPhoneNumber(String phoneNumber);
+
+    boolean existsByNickName(String nickName);
+    boolean existsByPhoneNumber(String phoneNumber);
+
+}
